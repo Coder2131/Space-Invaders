925a45783cf6e1306ad36279020aea546335e188
Coder2131 <95374410+Coder2131@users.noreply.github.com>
2023-03-23 10:34:47 -0400

    1 file modified, 4 added, 4 deleted

--- script.js
+++ script.js
@@ -8,35 +8,35 @@ class Player {
   constructor() {
     this.position = {
       x: 200,
-      y: 200,
+      y: 200
     }
 
     this.velocity = {
       x: 0,
-      y: 0,
+      y: 0
     }
 
-    const image = new Image();
-    image.src = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngegg.com%2Fen%2Fsearch%3Fq%3Dspace%2BInvaders&psig=AOvVaw2gkkKPVYEdrIm0ZJYHhNaM&ust=1679667484428000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLiAzo-f8v0CFQAAAAAdAAAAABAE'
+    const image = new Image()
+    image.src = './img/spaceship.png'
 
-    this.image = image;
-    this.width = 100;
-    this.height = 100;
+    this.image = image
+    this.width = 100
+    this.height = 100
   }
 
   draw() {
-    // c.fillStyle = 'red';
-    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    c.drawImage(this.image, this.position.x, this.position.y);
+    // c.fillStyle = 'red'
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
-const player = new Player();
-player.draw();
+const player = new Player()
+player.draw()
 
 function animate() {
-  requestAnimationFrame(animate);
-  player.draw();
+  requestAnimationFrame(animate)
+  player.draw()
 }
 
-animate();
+animate()
